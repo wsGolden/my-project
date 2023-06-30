34f8085545da51c5423664f33c94a34860a11bec
wangshuo <wangshuo2@liepin.com>
2023-06-30 14:48:05 +0800

    配置pm2命令

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,17 @@
 module.exports = {
   apps: [{
     name: 'wangshuo',
-    script: './server.js',
+    script: 'app/view/.next', // Next.js 脚本路径
     instances: 4,
     max_restarts: 7,
+    args: 'start', // 启动参数
+    exec_mode: 'cluster', // 使用集群模式
+    instances: 'max', // 根据 CPU 核心数量启动相应数量的实例
+    autorestart: true, // 自动重启进程
     env: {
       NODE_ENV: 'production',
       NODE_PORT: 8080,
       REACT_APP_ENV: 'test',
     },
-    watch: false,
-    merge_logs: true,
-    exec_mode: 'cluster',
-    max_memory_restart: '600M',
-    instance_var: 'NODE_APP_INSTANCE',
   }]
 }
