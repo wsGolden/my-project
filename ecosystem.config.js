eb39427dab76f888434b6571d1fc7257efc73b32
wsGolden <windowwangs@163.com>
2023-07-05 22:48:29 +0800

    修改启动命令和配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,10 @@
 module.exports = {
   apps: [{
     name: 'wangshuo',
-    script: 'app/view/.next', // Next.js 脚本路径
-    instances: 4,
-    max_restarts: 7,
-    args: 'start', // 启动参数
-    exec_mode: 'cluster', // 使用集群模式
-    instances: 'max', // 根据 CPU 核心数量启动相应数量的实例
-    autorestart: true, // 自动重启进程,
+    script: 'npm run next:start', // Next.js 脚本路径
     log_file: './logs/app.log', // 日志文件路径
     env: {
       NODE_ENV: 'production',
-      NODE_PORT: 8080,
-      REACT_APP_ENV: 'test',
     },
   }]
 }
